17f1b46bac8ccd60436bc96a252e548538a93581
Paulo-Pereira346 <paulpereira220@gmail.com>
2026-08-06 10:35:23 +0530

    Implemented Scheduler Functionality.

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,17 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+
 import { AppConfigModule } from './config/app-config.module';
 import { DatabaseModule } from './database/database.module';
 import { HealthModule } from './health/health.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { GithubModule } from './modules/github/github.module';
+import { SchedulerModule } from "./modules/scheduler/scheduler.module";
+
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
+
     AppConfigModule,
     DatabaseModule,
     HealthModule,
     AuthModule,
     GithubModule,
+    SchedulerModule
   ],
   controllers: [],
   providers: [],
--- apps/backend/src/modules/scheduler/scheduler.module.ts
+++ apps/backend/src/modules/scheduler/scheduler.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { SchedulerService } from './scheduler.service';
+
+import { GithubModule } from '../github/github.module';
+import { ConfigManagementModule } from '../config/config.module';
+import { LifecycleModule } from '../lifecycle/lifecycle.module';
+
+@Module({
+  imports: [
+    GithubModule,
+    ConfigManagementModule,
+    LifecycleModule,
+  ],
+  providers: [
+    SchedulerService,
+  ],
+})
 export class SchedulerModule {}
